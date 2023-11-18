9c96d32df7783f1bf28c0bca92222ec113c3f659
nacim <ouallanacim9@gmail.com>
2023-11-18 15:40:39 +0100

    bug fixed

--- player_class.py
+++ player_class.py
@@ -2,6 +2,10 @@ import random
 for i in range(1):
     if i == 0:
         ran_nu = random.randint(1,50)
+        while i == 0:
+            i+=1
+            rand_num = ran_nu
+        print("Random number is",ran_nu,rand_num)
 class player:
     def __init__(self, name,gender):
         self.name = name
@@ -18,3 +22,10 @@ class player_action:
     def move(self, action):
         self.move = action
         self.battle = action
+class Enemies:
+    def eni(self):
+        enemy = Enemies()
+        enemy.health = random.randint(1,5)
+        enemy.attack = random.randint(1,2)
+        enemy.name = "enemy"
+        return enemy
