20c44c1baa42e74a89b03d03e4be3b9bffb38b7b
ouallanacim06 <149337208+ouallanacim06@users.noreply.github.com>
2023-11-18 12:21:07 +0100

    update1 (#2)
    * update
    
    * mini update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from art import *
-from player_class import player
+from player_class import player,player_action
+import subprocess
 def main():
     title = text2art("QUEST QUASAR")
     print(title)
@@ -14,4 +15,15 @@ def main():
         print("wrong answer! try again.")
     pl_info = player(pl_name,pl_gender)
     print("\n\tWelcome to the world of Quest Quasar!\n")
-    
+    subprocess.run("cls",shell=True)
+    ch_num = text2art("chapitr 1")
+    print(ch_num + "\n\tThe first chapter is about a boy named "+str(pl_info.name))
+    print("who was born in some year he dont know anything about this world.\n")
+    print("one day he get summon to some tower with magical creature he see one personne and he say")
+    print("\twelcome irregulare")
+    print(str(pl_info.name)+ " : w-who are you? and where am i?")
+    print("unkown: you're in the tower and you're irruglare and if you want more answer you have to go to the top of this tower")
+    print("unkown: now you have to face this enimie")
+    print("you can choose between two options:\n")
+    pl_choise = int(input("1.attack\n"))
+main()
--- player_class.py
+++ player_class.py
@@ -1,14 +1,18 @@
+import random
+for i in range(1):
+    if i == 0:
+        ran_nu = random.randint(1,50)
 class player:
     def __init__(self, name,gender):
         self.name = name
         self.xp = 0
         self.gender = gender
-        self.strength = 1
+        self.strength = ran_nu
         self.intelligence = 1
         self.agility = 1
         self.health = 50
         self.max_health = 50
-        
+
     
 class player_action:
     def move(self, action):
